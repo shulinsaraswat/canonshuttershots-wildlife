67ee07befc986a134055bb152c9c2f3e0369f996
shulin59 <shulin59@gmail.com>
2020-11-30 12:35:05 +0530

    commit on 30thNov

--- src/component/Wildlife.js
+++ src/component/Wildlife.js
@@ -41,59 +41,125 @@ export default function Wildlife(){
             <div className="container"><br/>
             <div className="row row-cols-1 row-cols-md-3 body-wl">
                 <div className="col mb-4">
-                    <div className="card box-wl">
+                    <div className="card box-wl hovereffect">
                     <img src={wl1} className="card-img-top" alt="Royal Bengal Tiger" />
+                    <div class="overlay">
+                        <h2>Bannerghatta, India</h2>
+                        <p>
+                            <span>Royal Bengal Tiger<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl">
+                    <div className="card box-wl hovereffect">
                     <img src={wl2} className="card-img-top" alt="Royal Bengal Tiger1"/>
+                    <div class="overlay">
+                        <h2>Bannerghatta, India</h2>
+                        <p>
+                            <span>Royal Bengal Tiger<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl">
+                    <div className="card box-wl hovereffect">
                     <img src={wl3} className="card-img-top" alt="Royal bengal tiger1"/>
+                    <div class="overlay">
+                        <h2>Bannerghatta, India</h2>
+                        <p>
+                            <span>Royal Bengal Tiger<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl wl6">
+                    <div className="card box-wl wl6 hovereffect">
                     <img src={wl6} className="card-img-top" alt="Bonnet Macaque"/>
+                    <div class="overlay">
+                        <h2>Hampi, India</h2>
+                        <p>
+                            <span>Bonnet Macaque<br/> Canon 450D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl">
+                    <div className="card box-wl hovereffect">
                     <img src={wl7} className="card-img-top" alt="Bonnet Macaque"/>
+                    <div class="overlay">
+                        <h2>Nandi Hills, India</h2>
+                        <p>
+                            <span>Bonnet Macaque<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl wl8">
+                    <div className="card box-wl wl8 hovereffect">
                     <img src={wl8} className="card-img-top" alt="Bonnet Macaque"/>
+                    <div class="overlay">
+                        <h2>Hampi, India</h2>
+                        <p>
+                            <span>Bonnet Macaque<br/> Canon 450D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl wl4">
+                    <div className="card box-wl wl4 hovereffect">
                     <img src={wl4} className="card-img-top" alt="Asiatic Lion"/>
+                    <div class="overlay">
+                        <h2>Bannerghatta, India</h2>
+                        <p>
+                            <span>Asiatic Lion<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl">
+                    <div className="card box-wl hovereffect">
                     <img src={wildlife} className="card-img-top" alt="Asiatic Lion"/>
+                    <div class="overlay">
+                        <h2>Bannerghatta, India</h2>
+                        <p>
+                            <span>Asiatic Lion<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl wl5">
+                    <div className="card box-wl wl5 hovereffect">
                     <img src={wl5} className="card-img-top" alt="Asiatic Lion"/>
+                    <div class="overlay">
+                        <h2>Bannerghatta, India</h2>
+                        <p>
+                            <span>Asiatic Lion<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-wl wl9">
+                    <div className="card box-wl wl9 hovereffect">
                     <img src={wl9} className="card-img-top" alt="Puppies"/>
+                    <div class="overlay">
+                        <h2>Bannerghatta, India</h2>
+                        <p>
+                            <span>Indian Pariah Dog<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4"></div>
                 <div className="col mb-4">
-                <div className="card box-wl wl10">
+                <div className="card box-wl wl10 hovereffect">
                     <img src={wl10} className="card-img-top" alt="Royal Bengal Tiger"/>
+                    <div class="overlay">
+                        <h2>Bannerghatta, India</h2>
+                        <p>
+                            <span>Royal Bengal Tiger<br/> Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
             </div>
